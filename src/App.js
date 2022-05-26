ef880fbf24e040668e06765d400dcce9ba411ab5
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-26 10:49:36 +0530

    App.js Updated -it will show home,about,contact section in page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact'
 function App() {
   return (
     <div className="App">
      <h1>Hello World!</h1>
+    <Home/>
+    <About/>
+    <Contact/>
     </div>
   );
 }
